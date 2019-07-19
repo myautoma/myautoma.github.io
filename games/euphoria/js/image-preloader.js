55abd081100760e631ba555b10c51484e9d0f5c8
myautoma <45327190+myautoma@users.noreply.github.com>
2019-07-19 15:34:19 +1200

    Add files via upload

---
+++ games/euphoria/js/image-preloader.js
@@ -0,0 +1,51 @@
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function(){
+        $('<img/>')[0].src = this;
+    });
+}
+
+preload([
+	'img/bazaarCard1.png',
+	'img/bazaarCard2.png',
+	'img/bazaarCard3.png',
+	'img/bazaarCard4.png',
+	'img/artifactMarket.png',
+	'img/breezeBar.png',
+	'img/camping.png',
+	'img/commodityArea.png',
+	'img/constructMarkets.png',
+	'img/factionAutoma.png',
+	'img/factionEuphorian.png',
+	'img/factionIcarite.png',
+	'img/factionSubterran.png',
+	'img/factionWastelander.png',
+	'img/skyLounge.png',
+	'img/help/artifactMarket.jpg',
+	'img/help/artifactMarket.png',
+	'img/help/automaCardDiagram.jpg',
+	'img/help/automaCardDiagram2.jpg',
+	'img/help/automaComponents.jpg',
+	'img/help/bazaarCard1.png',
+	'img/help/bazaarCard2.png',
+	'img/help/bazaarCard3.png',
+	'img/help/bazaarCard4.png',
+	'img/help/breezeBar.png',
+	'img/help/camping.png',
+	'img/help/commodityArea.png',
+	'img/help/constructMarket.jpg',
+	'img/help/constructMarkets.png',
+	'img/help/factionAutoma.png',
+	'img/help/factionEuphorian.png',
+	'img/help/factionIcarite.png',
+	'img/help/factionSubterran.png',
+	'img/help/factionWastelander.png',
+	'img/help/penalty.png',
+	'img/help/placeWorker.jpg',
+	'img/help/skyLounge.png',
+	'img/help/star.png',
+	'img/help/tiebreaker.jpg',
+	'../../img/closeHelp.png',
+	'../../img/menuClose.png',
+	'../../img/menuIcon.png',
+	'../../img/question.png'
+]);
