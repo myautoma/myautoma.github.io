b8abe8900d31ae0e0d5a4ae6bf8d07710f6a6de7
myautoma <45327190+myautoma@users.noreply.github.com>
2019-07-30 16:40:24 +1200

    Add files via upload

---
+++ games/pulsar2849/js/content.js
@@ -0,0 +1,41 @@
+var startContent = [
+    {
+        name: 'Setup',
+        id: 'setup',
+        content: '<p>Set up instructions here.</p>'
+    },
+    {
+        name: 'Gameplay',
+        id: 'gameplay',
+        content: '<p>Gameplay instructions here.</p>'
+    }
+
+    // Add any more starting information in the same syntax
+    
+]
+
+var helpContent = [
+    {
+        name: 'Setup',
+        id: 'setup',
+        classes: ['setup'],
+        content: '<p>Set up instructions here.</p>'
+    },
+    {
+        name: 'Gameplay',
+        id: 'gameplay',
+        classes: ['gameplay'],
+        content: '<p>Gameplay instructions here.</p>'
+    },
+    {
+        name: 'Credit',
+        id: 'credit',
+        content: '<p>This is not an official <a href="https://czechgames.com/" target="_blank" rel="noopener">CGE Games</a> product and has no affiliation with <a href="https://czechgames.com/" target="_blank" rel="noopener">CGE Games</a>. <a href="https://czechgames.com/en/pulsar-2849/" target="_blank" rel="noopener">Pulsar 2849</a> was designed by <a href="https://boardgamegeek.com/boardgamedesigner/9307/vladimir-suchy" target="_blank" rel="noopener">Vladimír Suchý</a>, with art by <a href="https://soeren-meding.com/" target="_blank" rel="noopener">Sören Meding</a>. All art in this app is from <a href="https://czechgames.com/en/pulsar-2849/" target="_blank" rel="noopener">Pulsar 2849</a>.</p>'
+
+    },
+    {
+        name: 'Contact',
+        id: 'contact',
+        content: '<p>Feel free to <a href="mailto:gavin.mcgruddy@gmail.com?subject=Pulsar%202849%20Automa" rel="noopener">email me</a>, or send a BGG message to <a href="https://boardgamegeek.com/geekmail/compose?touser=Gavmastaphlex" rel="noopener">Gavmastaphlex</a> with any feedbacks / suggestions.</p>'
+    }
+]
