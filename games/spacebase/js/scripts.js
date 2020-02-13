f08d2e01676a7932c428a1ab0384b79a7206a2c5
myautoma <45327190+myautoma@users.noreply.github.com>
2020-02-13 16:58:54 +1300

    Update scripts.js

--- games/spacebase/js/scripts.js
+++ games/spacebase/js/scripts.js
@@ -1,86 +1,54 @@
-var touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
-var winningScore = 0;
-var totalTurns = 0;
-var playerNum = 0;
-var aiRollNum = 0;
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Space Base</title>
 
-var players = ['Human', 'AI 1', 'AI 2', 'AI 3']
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/slider.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
 
-$(document).on(touchEvent, '#startGame', function() {
-  winningScore = $('#winningScoreSlider').val();
-  totalTurns = $('#aiTurnsSlider').val();
-  adjustUI();
-})
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma-extensions@4.0.0/dist/css/bulma-extensions.min.css">
 
-$(document).on(touchEvent, '#nextTurn', function() {
+	<script defer src="https://cdn.jsdelivr.net/npm/bulma-extensions@4.0.0/dist/js/bulma-extensions.min.js"></script>
+	
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
 
-  if(playerNum == 1) {
-    totalTurns--;
-    $('#turnNum span').html(totalTurns);
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129700564-17"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
 
-    if(totalTurns == 0) {
-      $('#buttons').hide();
-      $('#playerTurn').hide();
-      $('#diceInfo').hide();
-      $('#diceResults').html('<p class="has-text-danger title is-4">The AI Win!</p>')
-      return;
-    }
-  }
+	gtag('config', 'UA-129700564-17');
+	</script>
 
-  if(playerNum == 4) {
-    playerNum = 0;
-    let cardColumn = Math.floor(Math.random()*6) + 1;
-    $('#diceInfo').hide();
-    $('#diceResults').html('<p class="subtitle is-5">Refresh the cards in column ' + cardColumn + '.</p>')
-  } else {
-    $('#diceInfo').show();
-    $('#playerTurn span').html(players[playerNum]);
-    rollDice();
-    playerNum++;
-  }
 
-})
+</head>
+<body>
 
-function adjustUI() {
-  $('#sliderContainer').remove();
+	<div id="container">
 
-  var gameInfoHTML = '<p id="winningScore">Winning Score: <span class="bold">' + winningScore + '</span></p>';
-  gameInfoHTML += '<p id="turnNum">Turns Remaining: <span class="bold">' + totalTurns + '</span></p>';
-  gameInfoHTML += '<p id="playerTurn">Current Turn: <span class="bold has-text-link">' + players[playerNum] + '</span></p>';
+		<h1 id="mainTitle" class="title is-2">Space Base</h1>
+		<h2 class="subtitle is-4"><a href="https://boardgamegeek.com/thread/1990937/solo-variant" target="_blank">Solo Variant</a></h2>
 
-  $('#buttons').before(gameInfoHTML)
+		<div class="rows is-centered is-hcentered">
+			<div id="sliderContainer" class="row is-narrow has-text-centered">
+				<p class="subtitle is-4">AI Turns</p>
+				<input id="aiTurnsSlider" class="slider has-output is-fullwidth is-success is-large" step="5" min="10" max="30" value="25" type="range">
+				<output for="aiTurnsSlider">25</output>
+				<p class="subtitle is-4">Winning Score</p>
+				<input id="winningScoreSlider" class="slider has-output is-fullwidth is-warning is-large" step="10" min="40" max="70" value="40" type="range">
+				<output for="winningScoreSlider">40</output>
+			</div>
+			<div id="buttons" class="row is-narrow has-text-centered">
+				<button id="startGame" class="button is-link">Start Game</button>
+				<button id="nextTurn" class="button is-success">Next Turn</button>
+			</div>
+		</div>
 
-  var setupDiceResultsHTML = '<div id="diceResultsContainer">';
-  setupDiceResultsHTML += '<p id="diceInfo">Current Dice Roll:</p>';
-  setupDiceResultsHTML += '<div id="diceResults">';
-  setupDiceResultsHTML += '</div>';
-  setupDiceResultsHTML += '</div>';
+	</div>
 
-  $('#buttons').after(setupDiceResultsHTML)
-
-  $('#startGame').hide();
-  $('#nextTurn').show();
-
-  rollDice();
-  playerNum++;
-}
-
-function rollDice() {
-  let dice1 = Math.floor(Math.random()*6) + 1;
-  let dice2 = Math.floor(Math.random()*6) + 1;
-
-  var colorClass;
-
-  if(playerNum == 0) {
-    colorClass = 'is-info';
-    $('.has-text-danger').removeClass('has-text-danger').addClass('has-text-link')
-  } else {
-    colorClass = 'is-danger';
-    $('.has-text-link').removeClass('has-text-link').addClass('has-text-danger')
-  }
-
-  var diceResultsHTML = '<p class="subtitle is-4"><span class="dice tag ' + colorClass + ' is-large">' + dice1 + '</span><span class="dice tag ' + colorClass + ' is-large">' + dice2 + '</span></p>';
-
-  $('#diceResults').html(diceResultsHTML)
-
-}
+</body>
+</html>
