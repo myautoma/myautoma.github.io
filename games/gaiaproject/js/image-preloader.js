7d61013b3049f70915fc45b6a6bf38ebde95fb53
myautoma <45327190+myautoma@users.noreply.github.com>
2022-03-25 09:51:58 +1300

    Update image-preloader.js

--- games/gaiaproject/js/image-preloader.js
+++ games/gaiaproject/js/image-preloader.js
@@ -1,10 +1,18 @@
-function preload(arrayOfImages) {
-    $(arrayOfImages).each(function(){
-        $('<img/>')[0].src = this;
-    });
-}
-
-preload([
+const preload = src => new Promise(function(resolve, reject) {
+    const img = new Image();
+    img.onload = function() {
+      resolve(img);
+    }
+    img.onerror = reject;
+    img.src = src;
+  });
+  
+  const preloadAll = sources =>
+    Promise.all(
+      sources.map(
+        preload));
+  
+  const sources = [
 	'img/active-tile.png',
 	'img/blank-card.jpg',
     'img/endRoundGoalCircle.png',
@@ -210,4 +218,8 @@ preload([
 	'../../img/menuClose.png',
 	'../../img/menuIcon.png',
 	'../../img/question.png'
-]);
+];
+  
+preloadAll(sources)
+  .then(images => initiateGameElements())
+  .catch(err => console.error('Failed', err));
