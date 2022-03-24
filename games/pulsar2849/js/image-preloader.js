81478e4af1118f4eb420683bc1aa8849dd367416
myautoma <45327190+myautoma@users.noreply.github.com>
2022-03-25 09:53:04 +1300

    Update image-preloader.js

--- games/pulsar2849/js/image-preloader.js
+++ games/pulsar2849/js/image-preloader.js
@@ -1,10 +1,18 @@
-function preload(arrayOfImages) {
-    $(arrayOfImages).each(function(){
-        $('<img/>')[0].src = this;
-    });
-}
-
-preload([
+const preload = src => new Promise(function(resolve, reject) {
+    const img = new Image();
+    img.onload = function() {
+      resolve(img);
+    }
+    img.onerror = reject;
+    img.src = src;
+  });
+  
+  const preloadAll = sources =>
+    Promise.all(
+      sources.map(
+        preload));
+  
+  const sources = [
 	'img/stars.jpg',
 	'img/median.png',
 	'img/tick.png',
@@ -68,4 +76,8 @@ preload([
 	'../../img/menuClose.png',
 	'../../img/menuIcon.png',
 	'../../img/question.png'
-]);
+];
+  
+preloadAll(sources)
+  .then(images => initiateGameElements())
+  .catch(err => console.error('Failed', err));
