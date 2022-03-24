90d46214c863067740e8481ec8d4d014b979439b
myautoma <45327190+myautoma@users.noreply.github.com>
2022-03-25 09:05:49 +1300

    Add files via upload

---
+++ js/home-data.js
@@ -0,0 +1,224 @@
+var homePageData = {
+    automas: [
+        {
+            name: 'Clans of Caledonia',
+            pageTitle: 'Official Clans of Caledonia Automa',
+            id: 'clans-of-caledonia-official',
+            type: 'Official Automa',            
+            img: 'clans-of-caledonia.jpg',
+            linkType: 'external',
+            linkURL: 'https://cocautoma.github.io/',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7656604#item7656604',
+            tags: []
+        },{
+            name: 'Clans of Caledonia',
+            pageTitle: 'Unofficial Clans of Caledonia Automa',
+            id: 'clans-of-caledonia-unofficial',
+            type: 'Unofficial Automa',            
+            img: 'clans-of-caledonia.jpg',
+            linkType: 'internal',
+            linkURL: 'clansofcaledonia',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7652989#item7652989',
+            tags: []
+        },{
+            name: 'Euphoria',
+            pageTitle: 'Euphoria Automa',
+            id: 'Euphoria',
+            type: 'Official Automa',            
+            img: 'euphoria.jpg',
+            linkType: 'internal',
+            linkURL: 'euphoria',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7652990#item7652990',
+            tags: []
+        },{
+            name: 'Gaia Project',
+            pageTitle: 'Gaia Project Automa',
+            id: 'gaia-project',
+            type: 'Official Automa',            
+            img: 'gaia-project.jpg',
+            linkType: 'internal',
+            linkURL: 'gaiaproject',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7652991#item7652991',
+            tags: []
+        },{
+            name: 'Great Western Trail',
+            pageTitle: 'Great Western Trail Automa',
+            id: 'great-western-trail',
+            type: 'Unofficial Automa',            
+            img: 'great-western-trail.jpg',
+            linkType: 'internal',
+            linkURL: 'greatwesterntrail',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7652992#item7652992',
+            tags: []
+        },{
+            name: 'La Granja',
+            pageTitle: 'La Granja Automa',
+            id: 'la-granja',
+            type: 'Unofficial Automa',            
+            img: 'la-granja.jpg',
+            linkType: 'internal',
+            linkURL: 'lagranja',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7652993#item7652993',
+            tags: []
+        },{
+            name: 'Near and Far',
+            pageTitle: 'Near and Far Automa',
+            id: 'near-and-Far',
+            type: 'Unofficial Automa',            
+            img: 'near-and-far.jpg',
+            linkType: 'internal',
+            linkURL: 'nearandfar',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7652994#item7652994',
+            tags: []
+        },{
+            name: 'Orléans',
+            pageTitle: 'Orleans Automa',
+            id: 'orleans',
+            type: 'Unofficial Automa',            
+            img: 'orleans.jpg',
+            linkType: 'internal',
+            linkURL: 'orleans',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7652996#item7652996',
+            tags: []
+        },{
+            name: 'Pulsar 2849',
+            pageTitle: 'Pulsar 2849 Automa',
+            id: 'pulsar-2849',
+            type: 'Unofficial Automa',            
+            img: 'pulsar-2849.jpg',
+            linkType: 'internal',
+            linkURL: 'pulsar2849',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7652998#item7652998',
+            tags: []
+        },{
+            name: 'Space Base',
+            pageTitle: 'Space Base solo mode',
+            id: 'space-base',
+            type: 'Unofficial Automa',            
+            img: 'space-base.jpg',
+            linkType: 'internal',
+            linkURL: 'spacebase',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7652999#item7652999',
+            tags: []
+        },{
+            name: 'Star Wars: Outer Rim',
+            pageTitle: 'Star Wars: Outer Rim Automa',
+            id: 'star-wars-outer-rim',
+            type: 'Unofficial Automa',            
+            img: 'star-wars.jpg',
+            linkType: 'internal',
+            linkURL: 'starwarsouterrim',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7653000#item7653000',
+            tags: []
+        },{
+            name: 'Tapestry',
+            pageTitle: 'Tapestry Automa',
+            id: 'tapestry',
+            type: 'Official Automa',            
+            img: 'tapestry.jpg',
+            linkType: 'internal',
+            linkURL: 'tapestry',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7653001#item7653001',
+            tags: []
+        },{
+            name: 'Tapestry Expansions',
+            pageTitle: 'Tapestry Expansions Automa',
+            id: 'tapestry-expansions',
+            type: 'Official Automa',            
+            img: 'tapestry-expansions.jpg',
+            linkType: 'internal',
+            linkURL: 'tapestryexp',
+            bggLink: '',
+            tags: {
+                'content': 'Experimental',
+                'color': 'danger'
+            }
+        },{
+            name: 'Trajan',
+            pageTitle: 'Trajan Automa',
+            id: 'trajan',
+            type: 'Unofficial Automa',            
+            img: 'trajan.jpg',
+            linkType: 'internal',
+            linkURL: 'trajan',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7653003#item7653003',
+            tags: []
+        },{
+            name: 'Viticulture',
+            pageTitle: 'Viticulture Automa',
+            id: 'viticulture',
+            type: 'Official Automa',            
+            img: 'viticulture.jpg',
+            linkType: 'internal',
+            linkURL: 'viticulture',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7653004#item7653004',
+            tags: []
+        },{
+            name: 'Voyages of Marco Polo',
+            pageTitle: 'Voyages of Marco Polo Automa',
+            id: 'voyages-of-marco-polo',
+            type: 'Unofficial Automa',            
+            img: 'voyages-of-marco-polo.jpg',
+            linkType: 'internal',
+            linkURL: 'marcopolo',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7653005#item7653005',
+            tags: []
+        },{
+            name: 'Wingspan',
+            pageTitle: 'Wingspan Automa',
+            id: 'wingspan',
+            type: 'Official Automa',            
+            img: 'wingspan.jpg',
+            linkType: 'internal',
+            linkURL: 'wingspan',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7653006#item7653006',
+            tags: []
+        }
+    ],
+    games: [
+        {
+            name: 'Calico',
+            pageTitle: 'Calico game',
+            id: 'calico',
+            type: 'Solo demo',            
+            img: 'calico.jpg',
+            linkType: 'internal',
+            linkURL: 'calico',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7670098#item7670098',
+            tags: []
+        },{
+            name: 'Cascadia',
+            pageTitle: 'Cascadia game',
+            id: 'cascadia',
+            type: 'Solo demo',            
+            img: 'cascadia.jpg',
+            linkType: 'external',
+            linkURL: 'https://cascadiagame.github.io/',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7871134#item7871134',
+            tags: []
+        },{
+            name: 'Verdant',
+            pageTitle: 'Verdant game',
+            id: 'verdant',
+            type: 'Solo demo',            
+            img: 'verdant.jpg',
+            linkType: 'external',
+            linkURL: 'https://verdantgame.github.io/',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/8511623#item8511623',
+            tags: []
+        }
+    ],
+    tools: [
+        {
+            name: 'PARKS',
+            pageTitle: 'PARKS Trail Randomizer',
+            id: 'parks',
+            type: 'Trail Randomizer',            
+            img: 'parks.jpg',
+            linkType: 'internal',
+            linkURL: 'parks',
+            bggLink: 'https://boardgamegeek.com/geeklist/272024/item/7652997#item7652997',
+            tags: []
+        }
+    ]
+}
